7ff3a3675b1b62726f2ae8c135dd3dc0faab9a97
Rich20067 <ruizricardo504@gmail.com>
2024-10-07 05:32:08 -0500

    ..

--- resources/views/faqs/faqs_recurso.blade.php
+++ resources/views/faqs/faqs_recurso.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.layoutLogin')
 <link rel="stylesheet" href="../../css/faqs.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 @section('nav')
     <nav>
     </nav>
@@ -10,23 +11,54 @@
         <h1>FAQs</h1>
             <div class="faq-sections">
                 <div class="faq-category">
-                    <h2>Soporte al cliente</h2>
+                    <h2>Preguntas generales</h2>
                     <ul>
-                        <li>¿Qué servicios legales ofrecen? <img src="../../img/mas.png" alt="mas" class="plus-icon"></li>
-                        <li>¿Cómo puedo saber si necesito un abogado para mi situación legal? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cómo puedo obtener una copia de un documento legal importante? <img src="../../img/mas.png" class="plus-icon"></li>
+                        <li class="faq-item">¿Qué servicios legales ofrecen? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos una amplia gama de servicios legales que incluyen asesoramiento jurídico, representación en litigios, redacción de contratos, planificación patrimonial, resolución alternativa de disputas y más. Nuestros abogados tienen experiencia en diversas áreas del derecho, desde derecho corporativo hasta derecho de familia y derecho penal.</li>
+                        <li class="faq-item">¿Cómo puedo saber si necesito un abogado para mi situación legal? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Dependerá de la complejidad de tu caso y tu experiencia previa.</li>
+                        <li class="faq-item">¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">A través de mediación, arbitraje o negociación.</li>
+                        <li class="faq-item">¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Los plazos varían según el tipo de caso, te recomendamos consultar con un abogado.</li>
+                        <li class="faq-item">¿Cómo puedo obtener una copia de un documento legal importante? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Qué servicios legales ofrecen? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Cómo puedo saber si necesito un abogado para mi situación legal? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Cómo puedo obtener una copia de un documento legal importante? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
                     </ul>
                 </div>
                 <div class="faq-category">
-                    <h2>Preguntas generales</h2>
+                    <h2>Soporte al cliente</h2>
                     <ul>
-                        <li>¿Qué servicios legales ofrecen? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cómo puedo saber si necesito un abogado para mi situación legal? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <img src="../../img/mas.png" class="plus-icon"></li>
-                        <li>¿Cómo puedo obtener una copia de un documento legal importante? <img src="../../img/mas.png" class="plus-icon"></li>
+                        <li class="faq-item">¿Qué servicios legales ofrecen? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos una amplia gama de servicios legales que incluyen asesoramiento jurídico, representación en litigios, redacción de contratos, planificación patrimonial, resolución alternativa de disputas y más. Nuestros abogados tienen experiencia en diversas áreas del derecho, desde derecho corporativo hasta derecho
+                            de familia y derecho penal.</li>
+                        <li class="faq-item">¿Cómo puedo saber si necesito un abogado para mi situación legal? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Dependerá de la complejidad de tu caso y tu experiencia previa.</li>
+                        <li class="faq-item">¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">A través de mediación, arbitraje o negociación.</li>
+                        <li class="faq-item">¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Los plazos varían según el tipo de caso, te recomendamos consultar con un abogado.</li>
+                        <li class="faq-item">¿Cómo puedo obtener una copia de un documento legal importante? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Puedes solicitar una copia a la entidad que emitió el documento o a través del abogado que gestionó tu caso.</li>
+                        <li class="faq-item">¿Qué servicios legales ofrecen? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos asesoramiento jurídico, representación en litigios, y más.</li>
+                        <li class="faq-item">¿Cómo puedo saber si necesito un abogado para mi situación legal? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos asesoramiento jurídico, representación en litigios, y más.</li>
+                        <li class="faq-item">¿Cuál es el proceso para resolver disputas legales fuera de los tribunales? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos asesoramiento jurídico, representación en litigios, y más.</li>
+                        <li class="faq-item">¿Cuáles son los plazos legales que debo tener en cuenta en mi caso? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos asesoramiento jurídico, representación en litigios, y más.</li>
+                        <li class="faq-item">¿Cómo puedo obtener una copia de un documento legal importante? <i class="fas fa-plus plus-icon"></i></li>
+                        <li class="faq-answer">Ofrecemos asesoramiento jurídico, representación en litigios, y más.</li>
                     </ul>
                 </div>
             </div>
